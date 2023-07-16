e69e3b43cd0162cdb21176f15fc0e119c2c28880
Abhishek <abhiyadav0719@gmail.com>
2023-07-16 16:03:57 +0530

    added carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import img1 from './components/lap.jpg'
+
 
 function App() {
   return (
      <>
 
      <Navbar/>
-      
+     <Carousel/>
+
      <h1>Hare Krishna</h1>
+
+    
+
+    
+      
+    
      </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg bg-primary">
       <div className="container-fluid">
-    <a className="navbar-brand text-white" href="/">Navbar</a>
+    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
